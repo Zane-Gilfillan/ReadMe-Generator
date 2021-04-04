a4704d4a93de2bc15ba96cbb39b57d421a1907d4
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-04-04 18:44:24 -0500

    set up basic prompt structure

---
+++ main.js
@@ -0,0 +1,45 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+inquirer.prompt(
+
+    [
+        {
+            type: 'input',
+            message: 'lorem',
+            name: 'lorem',
+        },
+        
+        {
+            type: 'input',
+            message: 'lorem',
+            name: 'lorem',
+        },
+
+        {
+            type: 'input',
+            message: 'lorem',
+            name: 'lorem',
+        },
+
+        {
+            type: 'input',
+            message: 'lorem',
+            name: 'lorem',
+        },
+
+        {
+            type: 'input',
+            message: 'lorem',
+            name: 'lorem',
+        },
+
+        {
+            type: 'input',
+            message: 'lorem',
+            name: 'lorem',
+        },
+
+
+    ]
+)
