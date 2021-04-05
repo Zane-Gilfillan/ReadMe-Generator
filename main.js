3eb3a6d040e60a731bd2ca6941281aea4399efff
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-04-05 13:03:11 -0500

    set up inquirer promp

--- main.js
+++ main.js
@@ -4,42 +4,56 @@ const fs = require('fs')
 inquirer.prompt(
 
     [
+
         {
             type: 'input',
-            message: 'lorem',
-            name: 'lorem',
+            message: 'what is your github username?',
+            name: 'user',
+        },
+
+        {
+            type: 'input',
+            message: 'what is the name of your project?',
+            name: 'title',
         },
         
         {
             type: 'input',
-            message: 'lorem',
-            name: 'lorem',
+            message: 'tell me a little bit about your project',
+            name: 'description',
         },
 
         {
             type: 'input',
-            message: 'lorem',
-            name: 'lorem',
+            message: 'are there any installation instructions?',
+            name: 'install',
         },
 
         {
             type: 'input',
-            message: 'lorem',
-            name: 'lorem',
+            message: 'are there any usage information',
+            name: 'usage',
         },
 
         {
             type: 'input',
-            message: 'lorem',
-            name: 'lorem',
+            message: 'are there any contributors to this project?',
+            name: 'contrib',
         },
 
         {
             type: 'input',
-            message: 'lorem',
-            name: 'lorem',
+            message: 'are there any test instructions?',
+            name: 'test',
         },
 
-
+        {
+            type: 'input',
+            message: 'choose a license for your project',
+            name: 'license',
+        },
     ]
-)
+).then( ({ user, title, description, install, usage, contrib, test, license}) => {
+
+})
+
